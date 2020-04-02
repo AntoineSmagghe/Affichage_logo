e56b1c16525cf2217e1057974296b9124ec31746
Antoine Smagghe <antoine@smagghe.me>
2020-04-02 08:52:06 +0200

    try a windows batch version - 25%

---
+++ logo.bat
@@ -0,0 +1,4 @@
+echo \<style\>#logos{display:flex;flex-wrap:wrap;width:1113px;margin:auto;justify-content:center}.logo{width:228px;object-fit:scale-down;max-height:110px;padding:15px;display:flex;margin:auto 0}\<\/style\> > test.html
+echo \<section id='logos'\> >> test.html
+for %%a in (*.ink) do echo \<img src='%%a' class='logo'\> >> test.html
+echo \<\/section\> >> test.html
