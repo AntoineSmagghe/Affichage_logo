a78eb2ff26b8611afb6e008180692e57a56e6aa6
Antoine Smagghe <antoine@smagghe.me>
2020-04-10 18:30:03 +0200

    add class to generate multiple html in multiple folders

---
+++ class/createMultipleFiles.php
@@ -0,0 +1,39 @@
+<?php
+
+include "links.php";
+
+for ($i = 0; $i < count($link); $i++){
+    
+    ob_start();
+    include "index.php";
+    $conent = ob_get_clean();
+
+    mkdir("emailings/" . $link[$i]["name"]);
+    mkdir("emailings/" . $link[$i]["name"] . "/img");
+    file_put_contents("emailings/" . $link[$i]['name'] . "/Emailing_01" . $link[$i]["name"] . ".html", $content);
+
+}
+
+echo "Job is done!";
+
+
+/*
+class CreateMultipleFiles
+{
+    private $data;
+    private $templatePath;
+    
+    private function __construct($data, $templatePath){
+        $this->data = $data;
+        $this->templatePath = $templatePath;
+    }
+
+    public function builder(){
+        ob_start();
+        include $this->templatePath;
+        $conent = ob_get_clean();
+
+
+    }
+}
+*/
