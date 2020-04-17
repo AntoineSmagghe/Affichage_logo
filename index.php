3fffd8ef3a076154b4dcd5e0c7381c8f2897bc91
Antoine Smagghe <antoine@smagghe.me>
2020-04-17 11:55:40 +0200

    add javascript for resize images

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
     justify-content: space-around;
 }
 
+.logo_container{
+}
+
 .logo{
     width: 228px;
-    object-fit: scale-down;
     max-height: 110px;
-    padding: 15px;
-    display: flex;
+    object-fit: scale-down;
+    padding: 15px;  
     margin: auto 0;
 }
 </style>
@@ -31,11 +33,63 @@ try{
     echo $e;
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "<pre>LISTE DES LOGOS : " . print_r(array_filter($output), true) . "</pre>"; ?>
 
-<?php echo "<pre>LISTE DES LOGOS : " . print_r(array_filter($output), true) . "</pre>"; ?>
+    <section id="logos">
+        <?php for ($i = 0; $i < (count($output) - 1); $i++){ ?>
+        <!--div class="logo_container"-->
+            <img src="<?= $output[$i] ?>" class="logo">
+        <!--/div-->
+        <?php } ?>
+    </section>
+    <script>
+        var elements = document.querySelectorAll(".logo_container");
+        
+        for (let i = 0; i < elements.length; i++){
+            //create box in bottom-left
+            var resizer = document.createElement('div');
+            resizer.class = "resizerElt";
+            resizer.style.width = '10px';
+            resizer.style.height = '10px';
+            resizer.style.background = 'red';
+            resizer.style.position = 'absolute';
+            resizer.style.right = 0;
+            resizer.style.bottom = 0;
+            resizer.style.cursor = 'se-resize';
+            //Append Child to Element
+            elements[i].appendChild(resizer);
+            //box function onmousemove
+            resizer.addEventListener('mousedown', initResize, false);
 
-<section id="logos">
-    <?php for ($i = 0; $i < (count($output) - 1); $i++){ ?>
-        <img src="<?= $output[$i] ?>" class="logo">
-    <?php } ?>
-</section>
+            //Window funtion mousemove & mouseup
+            function initResize(e) {
+                window.addEventListener('mousemove', Resize, false);
+                window.addEventListener('mouseup', stopResize, false);
+            }
+
+            //resize the element
+            function Resize(e) {
+                elements[i].style.width = (e.clientX - elements[i].offsetLeft) + 'px';
+                elements[i].style.height = (e.clientY - elements[i].offsetTop) + 'px';
+            }
+            
+            //on mouseup remove windows functions mousemove & mouseup
+            function stopResize(e) {
+                window.removeEventListener('mousemove', Resize, false);
+                window.removeEventListener('mouseup', stopResize, false);
+            }
+        }
+    </script>
+</body>
+</html>
+
+
+
